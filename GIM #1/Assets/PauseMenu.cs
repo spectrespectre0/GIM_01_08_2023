7d93f04276e5513a7f38b518077bdbbaadf8f377
stevennowen <owenliauw05@gmail.com>
2024-01-09 15:21:04 +0700

    Main Menu , Pause , GoBack
    1. Add Main Page
    2. Add Pause and Resume
    3. Add Go Back button to Main Page

---
+++ GIM #1/Assets/PauseMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    public void Pause()
+    {
+      pauseMenu.SetActive(true);
+      Time.timeScale = 0;
+    }
+    public void Resume()
+    {
+      pauseMenu.SetActive(false);
+      Time.timeScale = 1;
+    } 
+    public void Goback()
+    {
+        SceneManager.LoadSceneAsync(0);
+        Time.timeScale = 1; 
+    }
+
+
+}
